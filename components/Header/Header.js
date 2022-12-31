e19239f72d34774c8ec74f71cde521ef72e6b35e
nayanrdeveloper <nayanrdeveloper@gmail.com>
2022-12-31 13:03:42 +0530

    [init] Project inilization

---
+++ components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        {/* Left */}
+        <div></div>
+
+        {/* Center */}
+        <div></div>
+
+        {/* Right */}
+        <div></div>
+
+    </header>
+  )
+}
